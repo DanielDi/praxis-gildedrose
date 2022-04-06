08f8d05e027fbe98148eb27cf95a9d008587450a
DanielDi <despinalm@unal.edu.co>
2022-04-06 01:32:55 -0500

    vagrant environment variables

--- settings.sh
+++ settings.sh
@@ -7,6 +7,10 @@ echo 'export JAVA_HOME=/usr/lib/jvm/java-1.17.0-openjdk-amd64' >> /home/vagrant/
 echo 'export PATHADD=$JAVA_HOME/bin' >> /home/vagrant/.bashrc
 echo 'export PATH=$PATH:$PATHADD' >> /home/vagrant/.bashrc
 
+export JAVA_HOME=/usr/lib/jvm/java-1.17.0-openjdk-amd64
+export PATHADD=$JAVA_HOME/bin
+export PATH=$PATH:$PATHADD
+
 echo "----- Installing Maven------"
 wget https://dlcdn.apache.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.tar.gz
 tar -xvf apache-maven-3.8.5-bin.tar.gz
@@ -16,3 +20,6 @@ echo 'export M2_HOME=/usr/lib/apache-maven-3.8.5' >> /home/vagrant/.bashrc
 echo 'export MAVEN_HOME=/usr/lib/apache-maven-3.8.5' >> /home/vagrant/.bashrc
 echo 'export PATH=${M2_HOME}/bin:${PATH}' >> /home/vagrant/.bashrc
 
+export M2_HOME=/usr/lib/apache-maven-3.8.5
+export MAVEN_HOME=/usr/lib/apache-maven-3.8.5
+export PATH=${M2_HOME}/bin:${PATH}
